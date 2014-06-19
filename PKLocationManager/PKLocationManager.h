41e1a6f7e2eb61a8b153c463cff6020612f63d95
Philip Kluz <Philip.Kluz@zuui.org>
2014-06-20 00:02:17 +0200

    Initial Commit

---
+++ PKLocationManager/PKLocationManager.h
@@ -0,0 +1,19 @@
+//
+//  PKLocationManager.h
+//  PKLocationManager
+//
+//  Created by Philip Kluz on 6/20/14.
+//  Copyright (c) 2014 NSExceptional. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PKLocationManager.
+FOUNDATION_EXPORT double PKLocationManagerVersionNumber;
+
+//! Project version string for PKLocationManager.
+FOUNDATION_EXPORT const unsigned char PKLocationManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PKLocationManager/PublicHeader.h>
+
+
