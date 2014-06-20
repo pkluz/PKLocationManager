9c8b6da7e465d9e2d82c951bd4d2f900f59364d3
Philip Kluz <Philip.Kluz@zuui.org>
2014-06-20 05:09:50 +0200

    Gitignore

---
+++ PKLocationManager/PKLocationMonitor.swift
@@ -0,0 +1,9 @@
+//
+//  PKLocationMonitor.swift
+//  PKLocationManager
+//
+//  Created by Philip Kluz on 6/20/14.
+//  Copyright (c) 2014 NSExceptional. All rights reserved.
+//
+
+import Foundation
